aaa4bbce28e80f52257ec3e351a767de6da879f6
Nerdy-boi <60608960+Nerdy-boi@users.noreply.github.com>
2022-11-29 20:58:30 +0000

    V2

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # YukiMenu
-A simple python based menu that canhelp with servermanagement by presenting a menu of commonly used commands
+A simple python based menu that can help with servermanagement by presenting a menu of commonly used commands
--- Yukimenu.py
+++ Yukimenu.py
@@ -1,34 +1,70 @@
 import os
 
-first_run = True
-menuv = {}
-menuv['1']="Pogfetch" 
-menuv['2']="Apt Update"
-menuv['3']="Pihole Update"
-menuv['4']="Chronometer"
-menuv['5']="Exit"
-options=menuv.keys()
-while(True):
-    if first_run ==True:
-        first_run = False
-        pass
-    else:
-        os.system('cls' if os.name == 'nt' else 'clear')
-    for entry in options: 
-        print(entry, menuv[entry])
-    selection=input("Please Select:") 
-    if selection =='1': 
-        os.system('cls' if os.name == 'nt' else 'clear')
-        os.system("neofetch --ascii ${HOME}/.config/neofetch/logo.txt")
-        input()
-    elif selection == '2': 
-        os.system("sudo apt update && apt upgrade")
-    elif selection == '3': 
-        os.system("pihole -up")
-    elif selection == '4':
-        os.system("pihole -c")
-    elif selection == '5':
-        exit()
 
-    else: 
-        print("Unknown Option Selected!")  
+clear_command = "cls" if os.name == "nt" else "clear"
+
+#my custom neofetch config.
+def pogfetch():
+    os.system(clear_command)
+    os.system("neofetch --ascii ${HOME}/.config/neofetch/logo.txt")
+    input()
+
+
+# Construct menu
+menu = {
+    "1": {
+        "name": "Pogfetch",
+        "func": pogfetch
+    },
+    "2": {
+        "name": "Update packages",
+        "func": lambda: os.system("sudo apt-get update && sudo apt-get upgrade")
+    },
+    "3": {
+        "name": "Update Pihole",
+        "func": lambda: os.system("pihole -up")
+    },
+    "4": {
+        "name": "Chronometer",
+        "func": lambda: os.system("pihole -c")
+    },
+    "5": {
+        "name": "Bash",
+        "func": lambda: (exit())
+    },
+}
+
+
+# This can be folded into the body of the if below the function
+def main():
+    keep_running = True
+    first_run = True
+    while keep_running:
+        # Clear the screen on first iteration
+        if first_run:
+            first_run = False
+
+        else:
+            os.system(clear_command)
+
+        # Display menu
+        for num, entry in menu.items():
+            print(num, entry["name"])
+
+        # Prompt user
+        selection = input("Please select:")
+
+        # Execute selection
+        if menu_entry := menu.get(selection, None):
+            menu_entry["func"]()
+
+        else:
+            print("Unknown option selected")
+
+    # When the loop is done
+    print("Exiting")
+
+
+# Realistically optional, but good practice
+if __name__ == "__main__":
+    main()
